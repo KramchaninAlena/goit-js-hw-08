011fe796bcaa741afd4a708587f19f12308c21e1
Alona <mykyshka2013@gmail.com>
2023-07-14 19:53:16 +0300

    task 02_03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+const currentTime = localStorage.getItem(STORAGE_KEY) ?? 0;
+
+player.setCurrentTime(currentTime);
+console.log(currentTime);
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+}, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
+let form = {};
+
+formEl.addEventListener('submit', onSubmitForm);
+formEl.addEventListener('input', throttle(onInputForm, 500));
+
+function onSubmitForm(evt) {
+    evt.preventDefault();
+    const {email, message} = evt.target.elements;
+        if(!email.value || !message.value){
+     return alert('Всі поля повинні бути заповнені')
+    };
+    const formData ={
+        email: email.value,
+        message: message.value,
+    }
+    formEl.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+};
+
+function onInputForm(evt) {
+   form[evt.target.name] = evt.target.value;
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(form));
+};
+
+function getObjValues () {
+    let userData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {}
+       for(const key in userData){
+        formEl[key].value = userData[key]
+    };    
+    };
+getObjValues()
 
